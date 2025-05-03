ee3c45510af7ba3c22d0970fe7cb3ded7d0b1663
Masaya-j9 <masaya.spectre@gmail.com>
2025-05-03 19:19:55 +0900

    add test settings

---
+++ src/pages/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { describe, it, expect } from "vitest";
+import Home from "./index";
+import React from "react";
+
+
+describe("Home Component", () => {
+  it('renders the heading with "Hello World"', () => {
+    render(<Home />);
+    const heading = screen.getByText("Hello World");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('renders a button with "Click Me"', () => {
+    render(<Home />);
+    const button = screen.getByText("Click Me");
+    expect(button).toBeInTheDocument();
+  });
+});
