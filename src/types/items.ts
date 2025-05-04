aad0aa17848c46f323724b425073851c4644dd54
Masaya-j9 <masaya.spectre@gmail.com>
2025-05-04 16:59:03 +0900

    add response types

---
+++ src/types/items.ts
@@ -0,0 +1,21 @@
+export type ItemCategory = {
+  id: number;
+  name: string;
+  itemId: number;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type Items = {
+  id: number;
+  name: string;
+  quantity: number;
+  description: string;
+  itemsCategories?: ItemCategory[];
+}
+
+export type ItemResponse = {
+  count: number;
+  results: Items[];
+}
