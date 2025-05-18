4124133d22352e73d6b139b0160d8d8b5b4efd98
Masaya-j9 <masaya.spectre@gmail.com>
2025-05-18 09:23:11 +0900

    add register schema

--- src/types/categories.ts
+++ src/types/categories.ts
@@ -1,5 +1,10 @@
 import { z } from "zod";
 
+const CATEGORY_NAME_MIN_LENGTH = 1;
+const CATEGORY_NAME_MAX_LENGTH = 255;
+const DESCRIPTION_MIN_LENGTH = 1;
+const DESCRIPTION_MAX_LENGTH = 255;
+
 export const CategorySchema = z.object({
   id: z.number(),
   name: z.string(),
@@ -8,6 +13,18 @@ export const CategorySchema = z.object({
   updatedAt: z.string().datetime(),
 });
 
+export const CategoryRegisterSchema = z.object({
+  name: z
+    .string()
+    .min(CATEGORY_NAME_MIN_LENGTH, 'カテゴリ名は必須です')
+    .max(CATEGORY_NAME_MAX_LENGTH, 'カテゴリ名は255文字以内で入力してください'),
+
+  description: z
+    .string()
+    .min(DESCRIPTION_MIN_LENGTH, '説明は必須です')
+    .max(DESCRIPTION_MAX_LENGTH, '説明は255文字以内で入力してください'),
+})
+
 export const CategoryResponseSchema = z.object({
   count: z.number(),
   categories: z.array(CategorySchema),
@@ -15,3 +32,4 @@ export const CategoryResponseSchema = z.object({
 
 export type Category = z.infer<typeof CategorySchema>;
 export type CategoryResponse = z.infer<typeof CategoryResponseSchema>;
+export type CategoryRegisterRequest = z.infer<typeof CategoryRegisterSchema>;
